b3657744e0657072aafdf543267e6074440fe3f8
melihadamla <damlatoprak@windowslive.com>
2026-03-31 00:01:16 +0300

    test(dispatcher): TDD Green asamasi tamamlandi, testler app objesine baglandi

--- dispatcher/tests/dispatcher.test.ts
+++ dispatcher/tests/dispatcher.test.ts
@@ -1,16 +1,17 @@
 import request from 'supertest';
 import { describe, it, expect } from '@jest/globals';
-
+import app from '../src/server'; 
 
 describe('Dispatcher (API Gateway) Testleri', () => {
     
     it('Sistem çalışıyor mu diye health check atıyoruz', async () => {
-        const response = await request('http://localhost:8080').get('/api/health');
+        const response = await request(app).get('/api/health');
         expect(response.status).toBe(200);
+        expect(response.text).toBe('OK'); 
     });
 
     it('Yanlış bir URL girilirse 404 hatası veriyor mu testi', async () => {
-        const response = await request('http://localhost:8080').get('/api/olmayan-rota');
+        const response = await request(app).get('/api/olmayan-rota');
         expect(response.status).toBe(404);
     });
 });
